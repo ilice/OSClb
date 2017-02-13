e6277491ef8edf1ee76be6ace23123023a323b51
teanocrata <teanocrata@gmail.com>
2017-02-13 16:10:09 +0100

    Errors in elastic and kibana ssl configuration

--- start.sh
+++ start.sh
@@ -4,10 +4,12 @@ sleep 3
 echo build starting nginx config
 
 
-echo replacing ___my.example.com___/$MY_DOMAIN_NAME
+echo replacing __www.opensmartcountry.com__/$OPENSMARTCOUNTRY_DOMAIN
+echo replacing __kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN
 
 # Put your domain name into the nginx reverse proxy config.
-sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/conf.d/osc.conf
+sed -i "s/__www.opensmartcountry.com__/$OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
+sed -i "s/__kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
 
 cat /etc/nginx/conf.d/osc.conf
 echo .
@@ -15,35 +17,61 @@ echo Firing up nginx in the background.
 nginx
 
 # Check user has specified domain name
-if [ -z "$MY_DOMAIN_NAME" ]; then
-    echo "Need to set MY_DOMAIN_NAME (to a letsencrypt-registered name)."
+if [ -z "$OPENSMARTCOUNTRY_DOMAIN" ]; then
+    echo "Need to set OPENSMARTCOUNTRY_DOMAIN (to a letsencrypt-registered name)."
+    exit 1
+fi
+
+# Check user has specified domain name
+if [ -z "$KIBANA_OPENSMARTCOUNTRY_DOMAIN" ]; then
+    echo "Need to set KIBANA_OPENSMARTCOUNTRY_DOMAIN (to a letsencrypt-registered name)."
     exit 1
 fi
 
 # This bit waits until the letsencrypt container has done its thing.
 # We see the changes here bceause there's a docker volume mapped.
-echo Waiting for folder /etc/letsencrypt/live/$MY_DOMAIN_NAME to exist
-while [ ! -d /etc/letsencrypt/live/$MY_DOMAIN_NAME ] ;
+echo Waiting for folder /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN to exist
+while [ ! -d /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN ] ;
 do
     sleep 2
 done
 
-while [ ! -f /etc/letsencrypt/live/$MY_DOMAIN_NAME/fullchain.pem ] ;
+echo Waiting for folder /etc/letsencrypt/live/$KIBANA_OPENSMARTCOUNTRY_DOMAIN to exist
+while [ ! -d /etc/letsencrypt/live/$KIBANA_OPENSMARTCOUNTRY_DOMAIN ] ;
 do
-        echo Waiting for file fullchain.pem to exist
     sleep 2
 done
 
-while [ ! -f /etc/letsencrypt/live/$MY_DOMAIN_NAME/privkey.pem ] ;
+while [ ! -f /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN/fullchain.pem ] ;
+do
+    echo Waiting for file fullchain.pem to exist
+    sleep 2
+done
+
+while [ ! -f /etc/letsencrypt/live/$KIBANA_OPENSMARTCOUNTRY_DOMAIN/fullchain.pem ] ;
+do
+    echo Waiting for file fullchain.pem to exist
+    sleep 2
+done
+
+while [ ! -f /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN/privkey.pem ] ;
 do
   echo Waiting for file privkey.pem to exist
-sleep 2
+  sleep 2
 done
 
-echo replacing ___my.example.com___/$MY_DOMAIN_NAME
+while [ ! -f /etc/letsencrypt/live/$KIBANA_OPENSMARTCOUNTRY_DOMAIN/privkey.pem ] ;
+do
+  echo Waiting for file privkey.pem to exist
+  sleep 2
+done
+
+echo replacing __www.opensmartcountry.com__/$OPENSMARTCOUNTRY_DOMAIN
+echo replacing __kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN
 
 # Put your domain name into the nginx reverse proxy config.
-sed -i "s/___my.example.com___/$MY_DOMAIN_NAME/g" /etc/nginx/osc-secure.conf
+sed -i "s/__www.opensmartcountry.com__/$OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
+sed -i "s/__kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
 
 #go!
 kill $(ps aux | grep '[n]ginx' | awk '{print $2}')

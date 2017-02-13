393ad94b92a6a4981274e946585ea2fbc3535ff1
Isabel Muñoz <teanocrata@gmail.com>
2017-02-13 08:38:40 +0100

    Simple Hello World

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM nginx:1.11
 
 COPY start.sh /
+RUN chmod +x /start.sh
 COPY nginx.conf /etc/nginx/
+ADD src/ /var/www
 
 RUN openssl dhparam -dsaparam -out /etc/ssl/private/dhparam.pem 4096
 
--- start.sh
+++ start.sh
@@ -23,11 +23,18 @@ sed -i "s/___APPLICATION_PORT___/$APP_PORT_80_TCP_PORT/g" /etc/nginx/nginx.conf
 cat /etc/nginx/nginx.conf
 echo .
 
-echo Firing up nginx in the background.
+echo Firing up nginx.
 nginx
 
-# Check user has specified domain name
+echo Check user has specified domain name
 if [ -z "$MY_DOMAIN_NAME" ]; then
     echo "Need to set MY_DOMAIN_NAME (to a letsencrypt-registered name)."
     # exit 1
 fi
+
+#go!
+kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
+cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf
+
+echo Firing up nginx in the background.
+nginx -g 'daemon off;'

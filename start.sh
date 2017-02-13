a8f4644280e35d6210f37c23e1b294f1dd784929
teanocrata <teanocrata@gmail.com>
2017-02-13 11:45:35 +0100

    typo

--- start.sh
+++ start.sh
@@ -3,10 +3,10 @@ sleep 3
 
 nginx_conf=/etc/nginx/nginx-osc.conf
 
-if [-f "$nginx_conf"] then;
+if [-f "$nginx_conf"]; then
   echo "$nginx_conf found."
   cp "$nginx_conf" /etc/nginx/nginx.conf
 fi
-  
+
 echo Firing up nginx in the background.
 nginx -g 'daemon off;'

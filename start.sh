bf209e2d8ab1c74cbe9af1251a95e30f70be8592
teanocrata <teanocrata@gmail.com>
2017-04-28 10:37:55 +0200

    Jupyter Notebook initial configuration

--- start.sh
+++ start.sh
@@ -7,11 +7,13 @@ echo build starting nginx config
 echo replacing __www.opensmartcountry.com__/$OPENSMARTCOUNTRY_DOMAIN
 echo replacing __kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN
 echo replacing __elastic.opensmartcountry.com__/$ELASTIC_OPENSMARTCOUNTRY_DOMAIN
+echo replacing __jupyter.opensmartcountry.com__/$JUPYTER_OPENSMARTCOUNTRY_DOMAIN
 
 # Put your domain name into the nginx reverse proxy config.
 sed -i "s/__www.opensmartcountry.com__/$OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
 sed -i "s/__kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
 sed -i "s/__elastic.opensmartcountry.com__/$ELASTIC_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
+sed -i "s/__jupyter.opensmartcountry.com__/$JUPYTER_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
 
 cat /etc/nginx/conf.d/osc.conf
 echo .
@@ -43,6 +45,11 @@ else
       echo "Need to set ELASTIC_OPENSMARTCOUNTRY_DOMAIN (to a letsencrypt-registered name)."
       exit 1
   fi
+
+  if [ -z "$JUPYTER_OPENSMARTCOUNTRY_DOMAIN" ]; then
+      echo "Need to set JUPYTER_OPENSMARTCOUNTRY_DOMAIN (to a letsencrypt-registered name)."
+      exit 1
+  fi
   # This bit waits until the letsencrypt container has done its thing.
   # We see the changes here bceause there's a docker volume mapped.
   echo Waiting for folder /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN to exist
@@ -70,11 +77,13 @@ else
 
   echo replacing __kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN
   echo replacing __elastic.opensmartcountry.com__/$ELASTIC_OPENSMARTCOUNTRY_DOMAIN
+  echo replacing __jupyter.opensmartcountry.com__/$JUPITER_OPENSMARTCOUNTRY_DOMAIN
 
   # Put your domain name into the nginx reverse proxy config.
 
   sed -i "s/__kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
   sed -i "s/__elastic.opensmartcountry.com__/$ELASTIC_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
+  sed -i "s/__jupyter.opensmartcountry.com__/$JUPYTER_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
 
   #go!
   kill $(ps aux | grep '[n]ginx' | awk '{print $2}')

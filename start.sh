5b264ff22976675f84810f75e882bcc41a59418c
teanocrata <teanocrata@gmail.com>
2017-08-23 20:10:42 +0200

    #208 Add api.opensmartcountry.com to nginx

--- start.sh
+++ start.sh
@@ -8,6 +8,7 @@ echo replacing __www.opensmartcountry.com__/$OPENSMARTCOUNTRY_DOMAIN
 echo replacing __kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN
 echo replacing __elastic.opensmartcountry.com__/$ELASTIC_OPENSMARTCOUNTRY_DOMAIN
 echo replacing __jupyter.opensmartcountry.com__/$JUPYTER_OPENSMARTCOUNTRY_DOMAIN
+echo replacing __api.opensmartcountry.com__/$API_OPENSMARTCOUNTRY_DOMAIN
 
 # Put your domain name into the nginx reverse proxy config.
 sed -i "s/__alowedIP__/$ALLOWED_IP/g" /etc/nginx/conf.d/osc.conf
@@ -15,6 +16,7 @@ sed -i "s/__www.opensmartcountry.com__/$OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/co
 sed -i "s/__kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
 sed -i "s/__elastic.opensmartcountry.com__/$ELASTIC_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
 sed -i "s/__jupyter.opensmartcountry.com__/$JUPYTER_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
+sed -i "s/__api.opensmartcountry.com__/$API_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/conf.d/osc.conf
 
 cat /etc/nginx/conf.d/osc.conf
 echo .
@@ -51,6 +53,11 @@ else
       echo "Need to set JUPYTER_OPENSMARTCOUNTRY_DOMAIN (to a letsencrypt-registered name)."
       exit 1
   fi
+
+  if [ -z "$API_OPENSMARTCOUNTRY_DOMAIN" ]; then
+      echo "Need to set API_OPENSMARTCOUNTRY_DOMAIN (to a letsencrypt-registered name)."
+      exit 1
+  fi
   # This bit waits until the letsencrypt container has done its thing.
   # We see the changes here bceause there's a docker volume mapped.
   echo Waiting for folder /etc/letsencrypt/live/$OPENSMARTCOUNTRY_DOMAIN to exist
@@ -83,12 +90,14 @@ else
   echo replacing __kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN
   echo replacing __elastic.opensmartcountry.com__/$ELASTIC_OPENSMARTCOUNTRY_DOMAIN
   echo replacing __jupyter.opensmartcountry.com__/$JUPITER_OPENSMARTCOUNTRY_DOMAIN
+  echo replacing __api.opensmartcountry.com__/$API_OPENSMARTCOUNTRY_DOMAIN
 
   # Put your domain name into the nginx reverse proxy config.
 
   sed -i "s/__kibana.opensmartcountry.com__/$KIBANA_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
   sed -i "s/__elastic.opensmartcountry.com__/$ELASTIC_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
   sed -i "s/__jupyter.opensmartcountry.com__/$JUPYTER_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
+  sed -i "s/__api.opensmartcountry.com__/$API_OPENSMARTCOUNTRY_DOMAIN/g" /etc/nginx/osc-secure.conf
 
   #go!
   kill $(ps aux | grep '[n]ginx' | awk '{print $2}')
